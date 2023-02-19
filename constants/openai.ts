5f97ca78384a18635a7bf9eb9e67b096a19f3c72
Maxi <mpezzotta@folcode.com>
2023-02-19 19:13:21 -0300

    test .env

--- constants/openai.ts
+++ constants/openai.ts
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from 'openai'
 
 const configuration = new Configuration({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY
+  apiKey: process.env.PRIVATE_OPENAI_API_KEY
 })
 
 const OPEN_AI = new OpenAIApi(configuration)
