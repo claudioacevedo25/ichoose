acf84b2e69644aaf517e12979043299da4989fc1
Maxi <mpezzotta@folcode.com>
2023-02-19 18:36:06 -0300

    add new environments

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,13 +1,11 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: '495963441295-0gcstmonm72qq9ntm2si6qs1qt85v0fj.apps.googleusercontent.com',
-      clientSecret: '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'
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
     })
-    // ...add more providers here
   ]
 }
 export default NextAuth(authOptions)
--- types/environments.d.ts
+++ types/environments.d.ts
@@ -1,5 +1,7 @@
 declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_OPENAI_API_KEY: string
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: string
+    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: string
   }
 }
