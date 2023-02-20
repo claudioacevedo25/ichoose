94003e6d98ba7d16602444a37c71247cc1510519
Maxi <mpezzotta@folcode.com>
2023-02-19 23:43:00 -0300

    delete hardcode footer

--- components/molecules/layout/layout.component.tsx
+++ components/molecules/layout/layout.component.tsx
@@ -10,12 +10,12 @@ type Props = {
 }
 export const Layout = ({ children }: Props) => {
   const { data: session, status } = useSession()
-  if (status === 'loading') return <div>CArgando SEssion</div>
+  if (status === 'loading') return <div>Loading Session</div>
   return (
     <>
       <Header />
       <main className={`${styles.container} ${inter.className}`}>{children}</main>
-      <div>footer</div>
+      <footer></footer>
     </>
   )
 }
